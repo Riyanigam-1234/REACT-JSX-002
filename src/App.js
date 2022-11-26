e4d18129a2dfe9dd36888014e1d4c916b7dbe499
Riya Nigam <97540322+Riyanigam-1234@users.noreply.github.com>
2022-11-26 16:04:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>The Evolution of Airbnb's Frontend</h1>
+        <p>@spikebrehm</p>
+        <img src="Airbnbs.png" className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
